9b35295e9f7055109ed91ac816ad0e49acbbb944
uijin31 <2pm0531@naver.com>
2024-12-09 16:47:18 +0900

    test: BookingServiceTest → BookingServiceUnitTest 클래스명 변경

--- nowait-api/src/test/java/com/nowait/application/BookingServiceUnitTest.java
+++ nowait-api/src/test/java/com/nowait/application/BookingServiceUnitTest.java
@@ -31,7 +31,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class BookingServiceTest {
+class BookingServiceUnitTest {
 
     @InjectMocks
     BookingService bookingService;
