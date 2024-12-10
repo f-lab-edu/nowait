82e8562885237bccdd2608c71156226ab6aafff6
uijin31 <2pm0531@naver.com>
2024-12-10 18:32:39 +0900

    chore: init.sql 작성

---
+++ nowait-api/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,52 @@
+-- Users
+CREATE TABLE users
+(
+    id         BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nickname   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP
+);
+
+-- Place
+CREATE TABLE place
+(
+    id           BIGINT PRIMARY KEY AUTO_INCREMENT,
+    name         VARCHAR(255) NOT NULL,
+    description  TEXT,
+    type         VARCHAR(30)  NOT NULL,
+    phone_number VARCHAR(20),
+    old_address  VARCHAR(255),
+    road_address VARCHAR(255),
+    created_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at   TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP
+);
+
+-- BookingSlot
+CREATE TABLE booking_slot
+(
+    id                BIGINT PRIMARY KEY AUTO_INCREMENT,
+    place_id          BIGINT    NOT NULL,
+    table_id          BIGINT    NOT NULL,
+    date              DATE      NOT NULL,
+    time              TIME      NOT NULL,
+    is_booked         BOOLEAN   NOT NULL,
+    deposit_required  BOOLEAN   NOT NULL,
+    confirm_required  BOOLEAN   NOT NULL,
+    deposit_policy_id BIGINT,
+    start_time        TIMESTAMP NOT NULL,
+    end_time          TIMESTAMP NOT NULL,
+    created_at        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at        TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP
+);
+
+-- Booking
+CREATE TABLE booking
+(
+    id              BIGINT PRIMARY KEY AUTO_INCREMENT,
+    booking_slot_id BIGINT      NOT NULL,
+    user_id         BIGINT      NOT NULL,
+    status          VARCHAR(30) NOT NULL,
+    party_size      INT,
+    created_at      TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at      TIMESTAMP   NOT NULL DEFAULT CURRENT_TIMESTAMP ON update CURRENT_TIMESTAMP
+);
