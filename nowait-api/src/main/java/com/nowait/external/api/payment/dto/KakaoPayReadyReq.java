a492a716a6159bf0ac39b36b8d11d64be5397130
uijin31 <2pm0531@naver.com>
2024-12-26 15:45:46 +0900

    feat: 카카오 페이 요청 및 응답 시 스네이크 케이스 변환 설정 추가

--- nowait-api/src/main/java/com/nowait/external/api/payment/dto/KakaoPayReadyReq.java
+++ nowait-api/src/main/java/com/nowait/external/api/payment/dto/KakaoPayReadyReq.java
@@ -1,18 +1,27 @@
 package com.nowait.external.api.payment.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.nowait.domain.model.booking.Booking;
 import com.nowait.external.api.payment.KakaoPayProperties;
 
 public record KakaoPayReadyReq(
     String cid,
+    @JsonProperty("partner_order_id")
     String partnerOrderId,
+    @JsonProperty("partner_user_id")
     String partnerUserId,
+    @JsonProperty("item_name")
     String itemName,
     int quantity,
+    @JsonProperty("total_amount")
     int totalAmount,
+    @JsonProperty("tax_free_amount")
     int taxFreeAmount,
+    @JsonProperty("approval_url")
     String approvalUrl,
+    @JsonProperty("cancel_url")
     String cancelUrl,
+    @JsonProperty("fail_url")
     String failUrl
 
 ) {
--- nowait-api/src/main/java/com/nowait/external/api/payment/dto/KakaoPayReadyRes.java
+++ nowait-api/src/main/java/com/nowait/external/api/payment/dto/KakaoPayReadyRes.java
@@ -1,14 +1,21 @@
 package com.nowait.external.api.payment.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 
 public record KakaoPayReadyRes(
     String tid,
+    @JsonProperty("next_redirect_app_url")
     String nextRedirectAppUrl,
+    @JsonProperty("next_redirect_mobile_url")
     String nextRedirectMobileUrl,
+    @JsonProperty("next_redirect_pc_url")
     String nextRedirectPcUrl,
+    @JsonProperty("android_app_scheme")
     String androidAppScheme,
+    @JsonProperty("ios_app_scheme")
     String iosAppScheme,
+    @JsonProperty("created_at")
     LocalDateTime createdAt
 ) {
 
