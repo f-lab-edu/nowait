e04b15b01a4895161674f227a59b79af98381d6b
uijin31 <2pm0531@naver.com>
2024-12-26 13:46:10 +0900

    test: 테스트 시 더미데이터를 추가하는 로직을 가독성 있게 분리
    - 각 메서드 실행 전에 실행되는 스크립트: sql/data.sql
    - 각 메서드 실행 후에 실행되는 스크립트: sql/cleanup.sql

--- nowait-api/src/test/java/com/nowait/booking/api/BookingApiIntegrationTest.java
+++ nowait-api/src/test/java/com/nowait/booking/api/BookingApiIntegrationTest.java
@@ -1,6 +1,7 @@
 package com.nowait.booking.api;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 import com.nowait.booking.dto.request.BookingReq;
 import com.nowait.booking.dto.response.BookingRes;
@@ -24,10 +25,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.web.util.UriComponentsBuilder;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@Sql(scripts = {"classpath:/sql/cleanup.sql", "classpath:/sql/data.sql"})
+@SqlGroup({
+    @Sql(scripts = "classpath:/sql/data.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD),
+    @Sql(scripts = "classpath:/sql/cleanup.sql", executionPhase = ExecutionPhase.AFTER_TEST_METHOD)
+})
 class BookingApiIntegrationTest {
 
     @Autowired
