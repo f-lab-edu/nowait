652790d5e8416945cef41751c1a0b1ed98dbb52b
uijin31 <2pm0531@naver.com>
2024-12-27 14:15:52 +0900

    rename: bookingId → paymentId 필드명 변경

--- nowait-api/src/main/java/com/nowait/controller/api/dto/request/ApproveDepositPaymentReq.java
+++ nowait-api/src/main/java/com/nowait/controller/api/dto/request/ApproveDepositPaymentReq.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull;
 
 public record ApproveDepositPaymentReq(
     @NotNull
-    Long bookingId,
+    Long paymentId,
     @NotBlank
     String pgToken
 ) {
