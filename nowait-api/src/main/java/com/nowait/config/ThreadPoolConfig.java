0926cfafb568bccb52a5410366cc7ded4f3752a3
uijin31 <2pm0531@naver.com>
2025-01-10 16:53:21 +0900

    chore: graceful shutdown 설정 추가
    - 스레드가 작업을 끝낼 때까지 30초를 기다린 후 종료되도록 설정

--- nowait-api/src/main/java/com/nowait/config/ThreadPoolConfig.java
+++ nowait-api/src/main/java/com/nowait/config/ThreadPoolConfig.java
@@ -3,6 +3,7 @@ package com.nowait.config;
 import jakarta.annotation.PreDestroy;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,14 +11,21 @@ import org.springframework.context.annotation.Configuration;
 public class ThreadPoolConfig {
 
     @Bean
-    public ExecutorService cachedThreadPool() {
+    public ExecutorService threadPool() {
         return Executors.newCachedThreadPool();
     }
 
     @PreDestroy
-    public void shutdownThreadPool() {
-        if (cachedThreadPool() != null && !cachedThreadPool().isShutdown()) {
-            cachedThreadPool().shutdown();
+    public void gracefulShutdown() {
+        if (threadPool() != null && !threadPool().isShutdown()) {
+            threadPool().shutdown();
+            try {
+                if (!threadPool().awaitTermination(30, TimeUnit.SECONDS)) {
+                    threadPool().shutdownNow();
+                }
+            } catch (InterruptedException e) {
+                threadPool().shutdownNow();
+            }
         }
     }
 }
