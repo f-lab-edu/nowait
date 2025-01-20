49de10ad86e49bd0b0acc99ed5c5a7a547373c1a
uijin31 <2pm0531@naver.com>
2025-01-20 17:53:59 +0900

    refactor: 불필요한 Getter 제거

--- nowait-api/src/main/java/com/nowait/config/PaymentProperties.java
+++ nowait-api/src/main/java/com/nowait/config/PaymentProperties.java
@@ -1,10 +1,8 @@
 package com.nowait.config;
 
-import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties("payment")
-@Getter
 public record PaymentProperties(
     int depositPaymentWaitHours,
     int approvalWaitMinutes
